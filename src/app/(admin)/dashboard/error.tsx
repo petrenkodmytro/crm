c4bc32f8b385fc200f6c734bc715cf0dfde122ea
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-01-22 14:01:09 +0200

    add error handling and nod-found

---
+++ src/app/(admin)/dashboard/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+// файл, що перехоплює помилки тільки для данної вкладки
+
+import React from 'react';
+import Button from '@/app/components/button';
+
+export interface ErrorComponentProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function ErrorComponent({ error, reset }: ErrorComponentProps) {
+  return (
+    <div>
+      <p>{`Something went wrong. ${error.message}`}</p>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
